a6f10b8a94f0bf9cd8851c99a03bb330d9ae1842
Stephen Leibbrandt <stephen@caincomputing.co.za>
2017-01-27 01:42:08 +0200

    Added function to parse the "To" list to an array sting for the JSON array and changed the SendSMS function to accept a dictionary which is parse to a JSON array.

--- Clickatell-Service/Api.cs
+++ Clickatell-Service/Api.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace Clickatell_Service
 {
     class Api
     {
-        public static string SendSMS(string Token, string JsonArray)
+        //This function is in charge of converting the data into a json array and sending it to the rest sending controller.
+        public static string SendSMS(string Token, Dictionary<string, string> Params)
         {
-            return Rest.Post(Token, JsonArray);
+            Params["to"] = CreateRecipientList(Params["to"]);
+            string JsonArray = JsonConvert.SerializeObject(Params, Formatting.Indented);
+            //return Rest.Post(Token, JsonArray);
+            return JsonArray;
+        }
+
+        //This function converts the recipients list into an array string so it can be parsed correctly by the json array.
+        public static string CreateRecipientList(string to)
+        {
+            string[] tmp = to.Split(',');
+            to = "[\"";
+            to = to + string.Join("\",\"", tmp);
+            to = to + "\"]";
+            return to;
         }
         
     }
